b20647d1b6106a2eccd69be3f7b7f263da518c87
MrLawbreaker <mrlawbreaker.github@gmail.com>
2015-03-13 05:17:15 +0100

    Added automatic Ancher change

--- FreeMoveChirper/FreeMoveChirper.cs
+++ FreeMoveChirper/FreeMoveChirper.cs
@@ -22,44 +22,44 @@ namespace FreeMoveChirper
     {
 
         private IChirper currentChirper;
+        private Vector2 currentChirperPos;
 
         private Vector2 defaultChirperPos;
         private Vector2 mousePosOnClick;
-        
-        private ChirperAnchor currentAnchor;
 
-        private bool clickedOnChirp = false;
+        //No matter what resolution the user has set the GUI will always 
+        //have this resolution (May change in future)
+        private float GUIWidth = 1920f;
+        private float GUIHeight = 1080f;
+
+        private bool leftClickedOnChirp = false;
+        private bool wasMoved = false;
 
         public override void OnCreated(IChirper c)
         {
             currentChirper = c;
             currentChirper.SetBuiltinChirperFree(true);
             defaultChirperPos = currentChirper.builtinChirperPosition;
-            currentAnchor = ChirperAnchor.TopCenter;
-            currentChirper.SetBuiltinChirperAnchor(currentAnchor);
+            currentChirperPos = currentChirper.builtinChirperPosition;
+            UpdateAnchor();
         }
 
         public override void OnUpdate()
         {
             //Test if player clicked on chirp
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(0))
             {
-                ChirpMessage.SendMessage(defaultChirperPos.ToString());
-
                 if (IsClickNearChirp(GetMousePos(), currentChirper.builtinChirperPosition))
-                    clickedOnChirp = true;
+                {
+                        leftClickedOnChirp = Input.GetMouseButtonDown(0);
+                }
 
                 mousePosOnClick = GetMousePos();
             }
 
-            //Has the player released the mouse button?
-            if (Input.GetMouseButtonUp(0) && clickedOnChirp)
-            {
-                clickedOnChirp = false;
-            }
 
             //Move chirper
-            if (Input.GetMouseButton(0) && clickedOnChirp)
+            if (Input.GetMouseButton(0) && leftClickedOnChirp)
             {
                 float minDistance = 15.0f; //Minimum distance the user need to drag the mouse for the chirper to move
                 float distanceMoved = ((GetMousePos()) - mousePosOnClick).magnitude;
@@ -67,26 +67,96 @@ namespace FreeMoveChirper
 
                 if (movedFarEnough)
                 {
-                    currentChirper.builtinChirperPosition = GetMousePos();
+                    ChirpPanel.instance.Collapse();
+                    wasMoved = true;
+                    SetChirpPosition(GetMousePos(), false);
                 }
             }
+            
+            
 
-            //Change chirper Anchor on rightclick
-            if (Input.GetMouseButtonUp(1))
+            //Has the player released the mouse button?
+            if (Input.GetMouseButtonUp(0) && leftClickedOnChirp && wasMoved)
             {
-                
+                leftClickedOnChirp = false;
+                wasMoved = false;
+
+                ChirpPanel.instance.Collapse();
+
+                UpdateAnchor();
             }
 
             //Reset chirper position on key combination
-            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.O))
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.O))
             {
-                currentChirper.builtinChirperPosition = defaultChirperPos;
+                SetChirpPosition(defaultChirperPos, true);
             }
+
+            currentChirperPos = currentChirper.builtinChirperPosition;
+        }
+
+        private void SetChirpPosition(Vector2 position, bool updateAnchor)
+        {
+            currentChirper.builtinChirperPosition = position;
+
+            currentChirperPos = position;
+            if(updateAnchor)
+                UpdateAnchor();
+        }
+
+
+        private void UpdateAnchor()
+        {
+            ChirperAnchor currentAnchor;
+            Vector2 currentPos = currentChirper.builtinChirperPosition;
+
+            float thirdOfScreenWidth = GUIWidth / 3;
+            float halfOfScreenHeight = GUIHeight / 2;
+
+            if (currentPos.y <= halfOfScreenHeight)
+            {
+                //Chirper is at the upper half of the screen
+                if (currentPos.x <= thirdOfScreenWidth)
+                {
+                    //Chirper is at the left side of the screen
+                    currentAnchor = ChirperAnchor.TopLeft;
+                }
+                else if (currentPos.x <= (thirdOfScreenWidth) * 2)
+                {
+                    //Chirper is at the middle of the screen
+                    currentAnchor = ChirperAnchor.TopCenter;
+                }
+                else
+                {
+                    //Chirper is at the right side of the screen
+                    currentAnchor = ChirperAnchor.TopRight;
+                }
+            }
+            else {
+                //Chirper is at the lower half of the screen
+                if (currentPos.x <= thirdOfScreenWidth)
+                {
+                    //Chirper is at the left side of the screen
+                    currentAnchor = ChirperAnchor.BottomLeft;
+                }
+                else if (currentPos.x <= (thirdOfScreenWidth) * 2)
+                {
+                    //Chirper is at the middle of the screen
+                    currentAnchor = ChirperAnchor.BottomCenter;
+                }
+                else
+                {
+                    //Chirper is at the right side of the screen
+                    currentAnchor = ChirperAnchor.BottomRight;
+                }
+            }
+
+            currentChirper.SetBuiltinChirperAnchor(currentAnchor);
         }
 
         private bool IsClickNearChirp(Vector2 mousePosOnClick, Vector2 chirperPosOnClick)
         {
-            float deltaDistance = 50.0f; //Maximum distance a click is registered on the chirper
+            float deltaDistance = 32.0f; //Maximum distance a click is registered on the chirper
 
             if ((chirperPosOnClick - mousePosOnClick).magnitude < deltaDistance)
                 return true;
@@ -98,17 +168,10 @@ namespace FreeMoveChirper
         {
             Vector2 mousePos = Event.current.mousePosition;
 
-            //No matter what resolution the user has set the GUI will always 
-            //have this resolution (May change in future)
-            float GUIWidth = 1920f;
-            float GUIHeight = 1080f;
-
-            mousePos.x *= (GUIWidth / (float)Screen.width);
-            mousePos.y *= (GUIHeight / (float)Screen.height);
+            mousePos.x *= (GUIWidth / Screen.width);
+            mousePos.y *= (GUIHeight / Screen.height);
 
             return mousePos;
         }
     }
-
-
 }
