b861941881cec13024ee21eeb8793979aae74ccb
MrLawbreaker <mrlawbreaker.github@gmail.com>
2015-03-13 18:52:10 +0100

    Fixed differen aspect ratios

--- FreeMoveChirper/FreeMoveChirper.cs
+++ FreeMoveChirper/FreeMoveChirper.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using ColossalFramework;
 using ICities;
+using ColossalFramework.Plugins;
 
 namespace FreeMoveChirper
 {
@@ -9,7 +10,7 @@ namespace FreeMoveChirper
     {
         public string Name
         {
-            get { return "Chirper Position Changer 1.2"; }
+            get { return "Chirper Position Changer 1.3"; }
         }
 
         public string Description
@@ -27,16 +28,21 @@ namespace FreeMoveChirper
         private Vector2 defaultChirperPos;
         private Vector2 mousePosOnClick;
 
-        //No matter what resolution the user has set the GUI will always 
-        //have this resolution (May change in future)
-        private float GUIWidth = 1920f;
+        //The GUI Resolution is dependent from the aspect ratio the user has set
+        private float GUIWidth = float.NaN;
         private float GUIHeight = 1080f;
 
+        private int ScreenWidth;
+        private int ScreenHeight;
+
+        private bool resSupported = true;
         private bool leftClickedOnChirp = false;
         private bool wasMoved = false;
 
         public override void OnCreated(IChirper c)
         {
+            SetScreenRes();
+            SetGUISize();
             currentChirper = c;
             currentChirper.SetBuiltinChirperFree(true);
             defaultChirperPos = currentChirper.builtinChirperPosition;
@@ -44,8 +50,56 @@ namespace FreeMoveChirper
             UpdateAnchor();
         }
 
+        private void SetScreenRes()
+        {
+            ScreenWidth = Screen.width;
+            ScreenHeight = Screen.height;
+        }
+
+        private void SetGUISize()
+        {
+            float ratio = (float)ScreenWidth / (float)ScreenHeight;
+            if (ratio >= 1.7)
+            {
+                //16:9
+                GUIWidth = 1920f;
+            }
+            else if (ratio >= 1.6)
+            {
+                //16:10
+                GUIWidth = 1730f;
+            }
+            else if (ratio >= 1.3f)
+            {
+                //4:3
+                GUIWidth = 1430f;
+            }
+            else
+            {
+                resSupported = false;
+            }
+        }
+
         public override void OnUpdate()
         {
+            //Check for change in Resolution size
+            if (Screen.width != ScreenWidth || Screen.height != ScreenHeight)
+            {
+                SetScreenRes();
+                SetGUISize();
+            }
+            
+            //Check if Screen Resolution is supported
+            if (!resSupported)
+            {
+                if (float.IsNaN(GUIWidth))
+                {
+                    ChirpMessage.SendMessage("FreeMoveChirperMod", "I am sorry but your resolution is not supported");
+                    GUIWidth = 0f;
+                }
+                return;
+            }
+
             //Test if player clicked on chirp
             if (Input.GetMouseButtonDown(0))
             {
@@ -168,8 +222,8 @@ namespace FreeMoveChirper
         {
             Vector2 mousePos = Event.current.mousePosition;
 
-            mousePos.x *= (GUIWidth / Screen.width);
-            mousePos.y *= (GUIHeight / Screen.height);
+            mousePos.x *= (GUIWidth / (float)ScreenWidth);
+            mousePos.y *= (GUIHeight / (float)ScreenHeight);
 
             return mousePos;
         }
