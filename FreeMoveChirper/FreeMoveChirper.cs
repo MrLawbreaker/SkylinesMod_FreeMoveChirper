55315fc110dce8d898c4d472cc7eb97e7626e720
MrLawbreaker <mrlawbreaker.github@gmail.com>
2015-03-15 19:19:23 +0100

    1.4.2

--- FreeMoveChirper/FreeMoveChirper.cs
+++ FreeMoveChirper/FreeMoveChirper.cs
@@ -11,7 +11,7 @@ namespace FreeMoveChirper
     {
         public string Name
         {
-            get { return "Chirper Position Changer 1.4.1"; }
+            get { return "Chirper Position Changer 1.4.2"; }
         }
 
         public string Description
